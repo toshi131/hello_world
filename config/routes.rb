a6d178c7876e2cf024abe2640e9e34b60a40a83f
ToshiSingh <singh.toshi131@gmail.com>
2021-07-26 03:33:49 +0530

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/
   root("static_pages#home")
   get "help"=>"static_pages#help"
   get "about"=>"static_pages#about"
   get "login"=>"static_pages#login"
   get "contact"=>"static_pages#contact"
-  get "signup"=>"static_pages#signup"
+  get "signup"=>"users#new"
 
 resources :users
 end
