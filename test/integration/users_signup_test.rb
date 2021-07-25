68e5228685f9b289d16f2f7991cff7c9c846f05d
ToshiSingh <singh.toshi131@gmail.com>
2021-07-26 01:01:51 +0530

    finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params:{user: { name:  "toshi"}}
+    end
+    
+  end
+end
